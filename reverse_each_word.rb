e0e3c1ad2a1596d4d6a18eb70ce15a98f3d7e679
Brian Desplantes <heftygac@gmail.com>
2020-02-19 01:24:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,8 @@
 def reverse_each_word (str)
   wrd_arr = str.split(" ")
-  i = 0
   reversed = []
   
-  # reversed = wrd_arr.collect {|word| word.reverse}
+  reversed = wrd_arr.collect {|word| word.reverse}
 
   reversed.join(" ")
 end
